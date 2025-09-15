018b20a7837c274b796cd4f72d29a7fa5ef4f880
shiemejaybalatero <shiemejaybalatero@gmail.com>
2025-09-15 21:06:00 +0800

    debugging js code

--- script.js
+++ script.js
@@ -186,10 +186,6 @@ function initializeProjectModal() {
     });
   });
 
-  closeModal.addEventListener("click", () => {
-    modal.style.display = "none";
-  });
-
   modal.addEventListener("click", (e) => {
     if (e.target === modal) {
       modal.style.display = "none";
